d5d9995a86c09a3e2cc628a8188e4fbba4262830
Pavithra R <pavr@microsoft.com>
2022-08-23 10:47:48 +0100

    Cr-2

--- Tailspin.SpaceGame.Web/Models/ErrorViewModel.cs
+++ Tailspin.SpaceGame.Web/Models/ErrorViewModel.cs
@@ -3,6 +3,8 @@ namespace TailSpin.SpaceGame.Web.Models
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
+        //stage
+        //CR-2
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
