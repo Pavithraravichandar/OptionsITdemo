e7eca0f59598bd199151415d6ae5932250f8c2c7
Pavithra R <Pavithra.R@microsoft.com>
2022-08-23 09:48:31 +0000

    Updated HomeController.cs

--- Tailspin.SpaceGame.Web/Controllers/HomeController.cs
+++ Tailspin.SpaceGame.Web/Controllers/HomeController.cs
@@ -13,6 +13,7 @@ namespace TailSpin.SpaceGame.Web.Controllers
     {
         // High score repository.
         //pull
+        //fetch
         private readonly IDocumentDBRepository<Score> _scoreRepository;
         // User profile repository.
         private readonly IDocumentDBRepository<Profile> _profileRespository;
